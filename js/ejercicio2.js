afe9181b3cb0a313cc2e384a716ff2e13d71a3b6
Agostina <lozagostina@gmail.com>
2022-07-20 19:57:46 -0300

    agrego ejercicio 2

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,1 +1,64 @@
-let ciudades= prompt('Ingresar nombre de ciudades')
+let salir = ''
+let ciudades = []
+
+while (salir == '') {
+    ciudad = prompt('Ingresar nombre de ciudades')
+    if (ciudad != null) {
+        ciudades.push(ciudad)
+    }
+    let seguir = confirm('Desea ingresar mas ciudades?')
+    if (!seguir) {
+        salir = 'y'
+    }
+}
+console.log(ciudades)
+document.write(ciudades);
+
+document.write('<br>Longitud del array;'+ ciudades.length)
+while (salir == '') {
+    ciudad = prompt('Ingresar nombre de ciudades')
+    if (ciudad != null) {
+        ciudades.push(ciudad)
+    }
+    let seguir = confirm('Desea ingresar mas ciudades?')
+    if (!seguir) {
+        salir = 'y'
+    }
+}
+console.log(ciudades)
+
+document.write('<br>Items posicionados primero, tercero y ultimo' + series[0, 2, 9])
+
+ciudades.push('Paris')
+document.write('<br>Agrego Paris al final del arreglo:<br>')
+while (salir == '') {
+    ciudad = prompt('Ingresar nombre de ciudades')
+    if (ciudad != null) {
+        ciudades.push(ciudad)
+    }
+    let seguir = confirm('Desea ingresar mas ciudades?')
+    if (!seguir) {
+        salir = 'y'
+    }
+}
+console.log(ciudades)
+document.write(ciudades)
+
+document.write('<br>Elemento que ocupa la segunda posicion' + ciudades[1])
+
+
+ciudades[1] = 'Barcelona'
+document.write('<br>Sustituyo el segundo elemento por Barcelona<br>')
+while (salir == '') {
+    ciudad = prompt('Ingresar nombre de ciudades')
+    if (ciudad != null) {
+        ciudades.push(ciudad)
+    }
+    let seguir = confirm('Desea ingresar mas ciudades?')
+    if (!seguir) {
+        salir = 'y'
+    }
+}
+console.log(ciudades)
+document.write(ciudades)
+
